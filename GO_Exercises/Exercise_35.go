895e75f2a2aa1709343fe34afbdd631c2c6d5818
Gowtham Girithar Srirangasamy <gowthamgirithar@gmail.com>
2019-08-08 13:41:22 +0530

    json & reflect with tags

---
+++ GO_Exercises/Exercise_35.go
@@ -0,0 +1,37 @@
+package main
+// tags and reflection
+import (
+	"fmt"
+	"reflect"
+)
+import "encoding/json"
+
+type TagExample struct{
+	Name string `json:"name" validation:"only alphabet"`     // it is not single quote and it is `   > very important
+	Age  int `json:"age"`
+	//This is because only fields starting with a capital letter are exported,
+	// or in other words visible outside the current package (and in the json package in this case). > very important
+}
+
+
+func main() {
+	dataType := reflect.TypeOf(TagExample{})  // get the tags through reflection
+	fieldTag, _ := dataType.FieldByName("name")  // get the field by name
+	println(fieldTag.Tag)
+	s, _ := fieldTag.Tag.Lookup("validation")
+	println(s)
+	s2, _ := fieldTag.Tag.Lookup("json")
+	println(s2)
+
+
+	//real world examples:
+	tagExample := TagExample{"Gowtham", 27}
+	jsonData, err := json.Marshal(tagExample)
+	if err == nil {
+		fmt.Printf(string(jsonData))
+	}
+
+
+
+
+}
