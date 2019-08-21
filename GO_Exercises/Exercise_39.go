89c1fd6461adb07fd9913ccc0de4b181f20cd490
Gowtham Girithar Srirangasamy <gowthamgirithar@gmail.com>
2019-08-21 21:21:42 +0530

    Update Exercise_39.go

--- GO_Exercises/Exercise_39.go
+++ GO_Exercises/Exercise_39.go
@@ -1,64 +1,21 @@
 package main
 
-type Organization struct {
-	Exist      bool
-	ProperName bool
-	HaveNumber bool
-}
+import (
+	"log"
+	"math/rand"
+	"time"
+)
 
-type FinalOrg func(org *Organization) (bool, error)
+func main()  {
+	log.SetFlags(0) //If you want to remove the formatting or change it, you can use the log.SetFlags function:
+	log.Println("test")
 
-func main() {
+	log.SetFlags(1)
+	log.Println("test")
 
-	a := func(org *Organization) {
-		org.HaveNumber = true
-	}
-	b := func(org *Organization) {
-		org.Exist = true
-	}
-	c := func(org *Organization) {
-		org.ProperName = true
+	rand.Seed(time.Now().UnixNano())
+	for i:=0 ; i<10 ; i++{
+		println(rand.Int())
 	}
 
-	//i can call a like
-	orgg := Organization{}
-	a(&orgg)
-
-	UpdateOrganization(a, b, c)
-
-	org := Organization{}
-	println(org.Exist)
-	//CALL THE ORGANIZATION EXIST AND RETURN
-	f := org.OrganizationExist()
-	f(&org)
-	println(org.Exist)
-	// function type implements
-	f.OrganizationReq(&org)
-	println(org.Exist)
-}
-
-//here functions are passed and each one will be executed if we use it
-//function as arguement
-func UpdateOrganization(functions ...func(organization *Organization)) bool {
-	org := Organization{}
-	for _, f := range functions {
-		f(&org)
-	}
-	println(org.ProperName, org.Exist, org.HaveNumber)
-	return org.HaveNumber
-}
-
-//return type as function type FinalOrg type
-// retyurn is function
-func (org Organization) OrganizationExist() FinalOrg {
-	return func(org *Organization) (bool, error) {
-		org.Exist = !org.Exist
-		return org.Exist, nil
-	}
-}
-
-//function type implements
-func (fo FinalOrg) OrganizationReq(organization *Organization) {
-	// here executing the function with org
-	fo(organization)
 }
